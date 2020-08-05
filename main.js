bebf4544529eb33508e7bc97cb0a1d12d37614a8
Zhifang Liu <zhifangliu@google.com>
2020-08-04 21:59:22 -0700

    ToyReact Day 1.

---
+++ main.js
@@ -0,0 +1,19 @@
+import {ToyReact, Component} from "./ToyReact.js"
+
+class MyComponent extends Component {
+    render() {
+        return <div>
+            <span>hello</span>
+            <span>world!</span>
+            <div>
+                {this.children}
+            </div>
+        </div>;
+    }
+}
+
+let a = <MyComponent name="a" id="ida">
+    <div>123</div>
+</MyComponent>
+
+ToyReact.render(a, document.body);
